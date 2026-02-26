cb8e4116db865ef6fbae6fa7850fddf26d18f680
MuhammadArsyaPradana <muhammadarsyapradana@gmail.com>
2026-02-26 12:51:22 +0700

    karyawan

---
+++ karyawan/lib/main.dart
@@ -0,0 +1,90 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:karyawan/models/karyawan.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      color: Colors.yellowAccent,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  Future<List<Karyawan>> loadKaryawan() async {
+    final String response = await rootBundle.loadString('assets/karyawan.json');
+    final List<dynamic> data = json.decode(response);
+
+    return data.map((json) => Karyawan.fromJson(json)).toList();
+  }
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Future<List<Karyawan>> _loadKaryawan;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadKaryawan = widget.loadKaryawan();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Daftar Karyawan")),
+      body: FutureBuilder<List<Karyawan>>(
+        future: _loadKaryawan,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                final karyawan = snapshot.data![index];
+                return ListTile(
+                  title: Text(
+                    karyawan.nama,
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Umur: ${karyawan.umur}"),
+                      Text(
+                        "Alamat: ${karyawan.alamat.jalan}, ${karyawan.alamat.kota}, ${karyawan.alamat.provinsi}",
+                      ),
+                      Text("Hobi: ${karyawan.hobi.join(', ')}"),
+                    ],
+                  ),
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Center(child: Text("Error: ${snapshot.error}"));
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
+  }
+}
